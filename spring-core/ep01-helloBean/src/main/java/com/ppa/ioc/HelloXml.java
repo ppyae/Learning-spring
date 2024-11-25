99489b6d038b9a088133a9086f037373f37d9b27
phyo <moehtat027@gmail.com>
2024-11-26 01:37:46 +0700

    xml bean

---
+++ spring-core/ep01-helloBean/src/main/java/com/ppa/ioc/HelloXml.java
@@ -0,0 +1,8 @@
+package com.ppa.ioc;
+
+public class HelloXml {
+
+	public void greet() {
+		System.out.println("Hello xml bean configuration");
+	}
+}
--- spring-core/ep01-helloBean/src/test/java/com/ppa/test/HelloTest.java
+++ spring-core/ep01-helloBean/src/test/java/com/ppa/test/HelloTest.java
@@ -2,8 +2,10 @@ package com.ppa.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.ppa.ioc.HelloBean;
+import com.ppa.ioc.HelloXml;
 
 public class HelloTest {
 
@@ -16,6 +18,19 @@ public class HelloTest {
 			
 			var bean = context.getBean(HelloBean.class);
 			bean.hello();
+		}
+	}
+	
+	@Test
+	void xmlTest() {
+		
+		try(var context = new GenericXmlApplicationContext("classpath:context.xml")){
+			
+//			context.load("classpath:context.xml");
+//			context.refresh();
+			
+			var bean = context.getBean(HelloXml.class);
+			bean.greet();
 		}
 	}
 }
