da70910749d9d2df95cae24a97b3304ccf40074a
phyo <moehtat027@gmail.com>
2024-12-03 02:12:52 +0700

    query for static sql

--- spring-jdbc/ep01-hello-jdbc/src/test/java/com/ppa/jdbc/test/MemberTest.java
+++ spring-jdbc/ep01-hello-jdbc/src/test/java/com/ppa/jdbc/test/MemberTest.java
@@ -1,7 +1,14 @@
 package com.ppa.jdbc.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
@@ -10,13 +17,18 @@ import com.ppa.jdbc.dao.MemberDao;
 import com.ppa.jdbc.dto.Member;
 
 @SpringJUnitConfig(classes = AppConfig.class)
+@TestMethodOrder(OrderAnnotation.class)
 class MemberTest {
 	
 	@Autowired
 	private MemberDao dao;
+	
+	@Autowired
+	private JdbcOperations operations;
 
 	@Test
 	@Sql(scripts = "/database.sql")
+	@Order(1)
 	void test() {
 		
 		Member m = new Member();
@@ -28,5 +40,11 @@ class MemberTest {
 		dao.create(m);
 	}
 
+	@Test
+	@Order(2)
+	void staticQuerySingleResultWithClass() {
+		var admin = operations.queryForObject("select name from member where loginId = 'admin'", String.class);
+		assertEquals("Pyae Phyo",admin);
+	}
 
 }
