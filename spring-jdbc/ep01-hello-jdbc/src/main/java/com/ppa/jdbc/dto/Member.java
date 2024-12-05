3e6d16f75ed46d09a100bea7531b3b53141c1488
phyo <moehtat027@gmail.com>
2024-12-06 03:35:56 +0700

    do Post mapping

--- spring-jdbc/ep01-hello-jdbc/src/main/java/com/ppa/jdbc/dto/Member.java
+++ spring-jdbc/ep01-hello-jdbc/src/main/java/com/ppa/jdbc/dto/Member.java
@@ -1,13 +1,36 @@
 package com.ppa.jdbc.dto;
 
-import lombok.Data;
-
-@Data
 public class Member {
 	
 	private String loginId;
 	private String name;
 	private String phone;
 	private String email;
+	public String getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 
 }
