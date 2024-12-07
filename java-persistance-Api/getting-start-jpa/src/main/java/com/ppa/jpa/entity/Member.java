5038c512a43f67e22be8f0697f85b6ed4893338e
phyo <moehtat027@gmail.com>
2024-12-08 03:05:40 +0700

    class level table mapping

--- java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
+++ java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
@@ -1,26 +1,39 @@
 package com.ppa.jpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "member_tb", uniqueConstraints = @UniqueConstraint(columnNames = {
+		"email" }))
+@SecondaryTable(name = "login_info", indexes = @Index(columnList = "role"))
 public class Member {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	private Contact contact;
+
+	@Column(table = "login_info")
+	private String LoginId;
+	@Column(table = "login_info")
+	private String password;
+	@Column(table = "login_info")
 	private Role role;
-	
-	
-	public enum Role{
-		Manager,Supervisor,Staff
+
+	private Contact contact;
+
+	public enum Role {
+		Manager, Supervisor, Staff
 	}
-	
 
 }
