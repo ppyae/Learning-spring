e235eb2c2741ea0d776eb05d5039822f7376a792
phyo <moehtat027@gmail.com>
2024-12-07 18:41:17 +0700

    embedable class

---
+++ java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Contact.java
@@ -0,0 +1,17 @@
+package com.ppa.jpa.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class Contact implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String phone;
+	private String email;
+	private String address;
+}
--- java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
+++ java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Data
@@ -16,7 +14,13 @@ public class Member {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	private String phone;
+	private Contact contact;
+	private Role role;
+	
+	
+	public enum Role{
+		Manager,Supervisor,Staff
+	}
 	
 
 }
