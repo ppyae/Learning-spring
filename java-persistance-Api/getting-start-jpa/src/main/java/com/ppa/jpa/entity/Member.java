8644d6bd97db23ebfe3f30062909e200d8112f35
phyo <moehtat027@gmail.com>
2024-12-11 18:35:35 +0700

    column mapping level annotation

--- java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
+++ java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Member.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,6 +36,7 @@ public class Member implements Serializable {
 	@Column(table = "login_info")
 	private String password;
 	@Column(table = "login_info")
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	private Contact contact;
--- java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Sales.java
+++ java-persistance-Api/getting-start-jpa/src/main/java/com/ppa/jpa/entity/Sales.java
@@ -1,9 +1,15 @@
 package com.ppa.jpa.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Entity
@@ -15,7 +21,15 @@ public class Sales implements Serializable {
 	@EmbeddedId
 	private SalePk pk;
 	private int id;
+	@Column(nullable = false)
 	private int sale;
+	@Column(name = "sale_date")
+	@Temporal(TemporalType.DATE)
+	private Date saleDate;
+	@Lob
 	private String description;
+	
+	@Transient
+	private boolean deleted;
 
 }
