537d7898b14d1ba01ef58a92d143c515ef6700bf
phyo <moehtat027@gmail.com>
2024-12-13 03:43:53 +0700

    configure collection MapKeyColumn

--- java-persistance-Api/jpa-collection/src/main/java/com/ppa/shop/entity/Product.java
+++ java-persistance-Api/jpa-collection/src/main/java/com/ppa/shop/entity/Product.java
@@ -3,11 +3,16 @@ package com.ppa.shop.entity;
 import java.util.List;
 import java.util.Map;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.MapKeyEnumerated;
 import lombok.Data;
 
 @Entity
@@ -20,6 +25,9 @@ public class Product {
 	private String name;
 	private String category;
 	@ElementCollection
+	@CollectionTable(name = "product_price", joinColumns = @JoinColumn(name = "product"))
+	@MapKeyColumn(name = "price_type")
+	@MapKeyEnumerated(EnumType.STRING)
 	private Map<PriceType, Integer> price;
 	@ElementCollection
 	private List<String> tags;
