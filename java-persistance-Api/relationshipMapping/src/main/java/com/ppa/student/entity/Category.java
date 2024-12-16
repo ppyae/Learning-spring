e7be85a46f989a5a77542f30e39c0d405d5f6108
phyo <moehtat027@gmail.com>
2024-12-17 01:53:06 +0700

    Mapby relation

---
+++ java-persistance-Api/relationshipMapping/src/main/java/com/ppa/student/entity/Category.java
@@ -0,0 +1,28 @@
+package com.ppa.student.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "category")
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	@ManyToMany
+	private List<Product> products;
+
+}
