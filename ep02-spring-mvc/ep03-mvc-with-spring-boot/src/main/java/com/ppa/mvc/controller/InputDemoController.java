b0e7f3bd8612f67932fe57fbefd8d06d6328db93
phyo <moehtat027@gmail.com>
2025-01-07 00:01:52 +0700

    Query Param Parameter

--- ep02-spring-mvc/ep03-mvc-with-spring-boot/src/main/java/com/ppa/mvc/controller/InputDemoController.java
+++ ep02-spring-mvc/ep03-mvc-with-spring-boot/src/main/java/com/ppa/mvc/controller/InputDemoController.java
@@ -3,6 +3,7 @@ package com.ppa.mvc.controller;
 import java.time.DayOfWeek;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,8 @@ public class InputDemoController {
 
 	//URL Get/inputs?day=MONDAY
 	@GetMapping
-	String inputValue(@RequestParam DayOfWeek day) {
+	String inputValue(@RequestParam DayOfWeek day,ModelMap model) {
+		model.put("day", day);
 		return "inputs/view1";
 	}
 }
